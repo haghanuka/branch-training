f1168a2ddb378d4a90074c51917c105f220ee260
UriMeyTal <urimeytal100@gmail.com>
2025-07-27 01:33:11 +0300

    egoeist

--- elevatorSubsystem.java
+++ elevatorSubsystem.java
@@ -1,28 +1,40 @@
-public class elevatorSubsystem {
-protected double MAX_SPEED=0.85 ;
-protected double MAX_HEIGHT = 0.6;
-protected double speed;
-protected double position = 0.0;
-
-protected double setspeed(double power){
-speed = power*MAX_SPEED;
-return speed;
-}
-
-protected double positionupdate(double currectspeed, double currectposition){
-    double distance = currectspeed*0.02;
-    position = currectposition+distance ;
-    return position;
-}
-
-protected boolean isup(double currectposition){
-    boolean isup = currectposition>=MAX_HEIGHT;
-    position = 0.6;
-    return isup;
-}
-protected boolean isdown(double currectposition){
-    boolean isdown = currectposition<=MAX_HEIGHT;
-    position = 0.0;
-    return isdown;
-}
-}
+public class elevatorSubsystem extends ledSubsystem  {
+    protected double MAX_SPEED=0.85 ;
+    protected double MAX_HEIGHT = 0.6;
+    protected double speed;
+    protected double position = 0.0;
+    
+    protected double setspeed(double power){
+    speed = power*MAX_SPEED;
+    return speed;
+    }
+    
+    protected double positionupdate(double currectspeed, double currectposition){
+        double distance = currectspeed*0.02; //מרחק=מהירות*זמן
+        position = currectposition+distance ;
+        if (position>currectposition){
+            super.oneColorblink("red");
+        }
+        else if (position<currectposition){
+            super.oneColorblink("green");
+        }
+        return position;
+    }
+    
+    protected boolean isup(double currectposition){
+        boolean isup = currectposition>=MAX_HEIGHT;
+        if (isup){
+            position = 0.6;
+            super.setColor("red");
+        }
+        return isup;
+    }
+    protected boolean isdown(double currectposition){
+        boolean isdown = currectposition<=0;
+        if (isdown){
+        position = 0.0;
+        super.setColor("green");
+    }
+        return isdown;
+    }
+    }
--- ledSubsystem.java
+++ ledSubsystem.java
@@ -1,5 +1,6 @@
 public class ledSubsystem {
         private String color; 
+        private int cycleCount = 0; 
     
         public ledSubsystem() {
             this.color = ""; 
@@ -16,6 +17,43 @@ public class ledSubsystem {
         public String getColor() {
             return this.color;
         }
+    
+        public void twoColorsBlink(String color1, String color2) {
+            cycleCount++;
+            if (cycleCount % 3 == 0) {
+                if (this.color.equals(color1)) {
+                    this.color = color2;
+                } else {
+                    this.color = color1;
+                }
+            }
     }
+            
+        public void oneColorBlink(String color) {
+                if (this.color.equals(color)) {
+                    this.color = "";
+                } else {
+                    this.color = color;
+                }
+            }
+             private int blinkRate = 2; 
+            private int cycleCount1 = 0;
+    public void twoColorsBlinkVariableRate(String color1, String color2) {
+        cycleCount++;
 
+        if (cycleCount % blinkRate == 0) {
+            if (color.equals(color1)) {
+                color = color2;
+            } else {
+                color = color1;
+            }
+        }
+
+        if (cycleCount1 % 6 == 0) {
+            blinkRate = (blinkRate == 2) ? 4 : 2;
+        }
+    }
+    
+        }
+    
 
