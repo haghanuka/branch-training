64ebb0567187418f7e929c17b9105891b7d0e2e7
ArielBader <arielelibader@gmail.com>
2025-07-26 21:41:37 +0300

    dd

--- elevatorSubsystem.java
+++ elevatorSubsystem.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 public class elevatorSubsystem {
-    public static main(string[] args){
+    public static void main(string[] args){
     Scanner scanner = new Scanner(system.in);
  double maxSpeed = 0.85; //m/s
  double maxHeight = 0.6 //m
     }
  public static double power (){
     System.out.print("enter power rate between 1 to -1");
-    double power = Scanner.scanner 
+    double power = scanner.nextouble();
+    return power;
  }
 }
