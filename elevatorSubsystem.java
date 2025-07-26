29cb109baf4675d72009913ad80f9008d7af016d
ArielBader <arielelibader@gmail.com>
2025-07-26 21:37:44 +0300

    start working on this class

---
+++ elevatorSubsystem.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class elevatorSubsystem {
+    public static main(string[] args){
+    Scanner scanner = new Scanner(system.in);
+ double maxSpeed = 0.85; //m/s
+ double maxHeight = 0.6 //m
+    }
+ public static double power (){
+    System.out.print("enter power rate between 1 to -1");
+    double power = Scanner.scanner 
+ }
+}
