18583d4f43ee1ac5e8e94eb95f9f78f5bfd2504d
UriAvn <avnon.uri@gmail.com>
2025-07-26 23:58:09 +0300

    pizza im zeytim vetiras

--- elevatorCommend.java
+++ elevatorCommend.java
@@ -1,16 +1,33 @@
-public interface elevatorCommend {
+public class elevatorCommend {
+public elevatorSubsystem elevatorSubsystem = new elevatorSubsystem();
+public ledSubsystem ledSubsystem = new ledSubsystem();
 public void elevatorGoUp () {
-    elevatorSubSystem.setSpeed(0.6);
+    elevatorSubsystem.setspeed(0.6);
     int i = 0;
-    while (elevatorSubSystem.position < elevatorSubSystem.MAX_HEIGHT) {
+    while (elevatorSubsystem.position < elevatorSubsystem.MAX_HEIGHT) {
         i++;
         System.out.println("the cycle number is: " + i);
         System.out.println("the time is: " + i*0.02);
-        elevatorSubSystem.setposition(speed, elevatorSubSystem.position)
-        System.out.println("the posision is: " + position);
+        elevatorSubsystem.positionupdate(elevatorSubsystem.speed, elevatorSubsystem.position);
+        System.out.println("the posision is: " + elevatorSubsystem.position);
     }
 }
+
+public void elevatorGoDownWithLED() {
+    elevatorSubsystem.setspeed(-0.6);
+
+    boolean ledState = false; // LED state: false = off, true = on
+    while (elevatorSubsystem.position > 0.0) {
+        ledState = !ledState; // Toggle LED state
+        elevatorSubsystem.positionupdate(elevatorSubsystem.speed, elevatorSubsystem.position);
+        System.out.println("the LED color is: " + ledSubsystem.getColor());
+    }
+}
+
 public static void main (String[] args) {
-    elevatorCommend.elevatorGoUp;
+    elevatorCommend elevatorCommend = new elevatorCommend();
+    elevatorCommend.elevatorGoUp();
 }
+
 }
+
