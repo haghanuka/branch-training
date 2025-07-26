e9a447f367c6504f5b59da13841d7251dcc4ed8a
UriMeyTal <urimeytal100@gmail.com>
2025-07-26 21:33:00 +0300

    changed the file

--- ledSubsystem.java
+++ ledSubsystem.java
@@ -1,4 +1,21 @@
 public class ledSubsystem {
+        private String color; 
     
+        public ledSubsystem() {
+            this.color = ""; 
+        }
+    
+        public void setColor(String color) {
+            this.color = color;
+        }
+    
+        public void turnOff() {
+            this.color = "";
+        }
+    
+        public String getColor() {
+            return this.color;
+        }
+    }
 
-}
+
